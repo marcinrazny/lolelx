7d723b96dfab69cfd70c77bd842428cf060ca2fb
SolfugaSoft <solfugasoft@gmail.com>
2020-06-02 23:00:35 +0200

    przebudowanie projektu w mavenie, poprawienie user.getFirstName() w profile.html, podpiecie listy uzytkownikow pod linkiem Ofert test, Musi być zalogowany Admin

--- src/main/java/com/cschool/marcinipiotrek/lolex/services/AdvertisementService.java
+++ src/main/java/com/cschool/marcinipiotrek/lolex/services/AdvertisementService.java
@@ -4,12 +4,11 @@ package com.cschool.marcinipiotrek.lolex.services;
 import com.cschool.marcinipiotrek.lolex.entities.Advertisement;
 import com.cschool.marcinipiotrek.lolex.entities.User;
 import com.cschool.marcinipiotrek.lolex.repositories.AdvertisementRepository;
-import jdk.internal.org.objectweb.asm.commons.AdviceAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class AdvertisementService {
